d059947c83fde33d09bc128773f69a5b24131a87
Christopher Brown <christopher.p.brown@gmail.com>
2016-06-30 15:20:54 -0600

    Create routes

---
+++ app/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+const Template = `
+<h3>Dashboard</h3>
+`;
+
+@Component({
+  selector: 'my-dashboard',
+  template: Template
+})
+export class DashboardComponent {}
